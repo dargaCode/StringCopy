97a6d546597e5ce12125b5701901c9726562c4b2
dargaCode <darga.code@gmail.com>
2015-10-24 19:27:24 -0400

    str_destroy function

--- copy.c
+++ copy.c
@@ -4,6 +4,7 @@
 // function prototypes
 int str_len(char* addr);
 void str_copy(char* addr_a, char* addr_b);
+void str_destroy(char* addr);
 
 int main(int argc, char* argv[])
 {
@@ -12,6 +13,11 @@ int main(int argc, char* argv[])
     char* duplicate = original;
 
     str_copy(original, duplicate);
+
+    printf("Original: %s \n", original);
+    printf("Duplicate: %s \n\n", duplicate);
+
+    str_destroy(original);
 
     printf("Original: %s \n", original);
     printf("Duplicate: %s \n\n", duplicate);
@@ -29,7 +35,17 @@ int str_len(char* addr)
 
 void str_copy(char* addr_a, char* addr_b)
 {
-    printf("\nCopying...\n");
+    printf("\nCopying string \"%s\"...\n", addr_a);
     int len = str_len(addr_a);
     addr_b = malloc(len * sizeof(char));
+}
+
+void str_destroy(char* addr)
+{
+    printf("\nDestroying string \"%s\"...\n", addr);
+    int len = str_len(addr);
+    for (int i = 0; i < len; i++)
+    {
+        *(addr + i) = 'x';
+    }
 }
